23e48eda0cff60d6cef65337282df5c22e3a8e74
Fida Zaman <88763525+FidaZaman@users.noreply.github.com>
2024-02-18 05:10:33 +0600

    arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -10,6 +10,11 @@ uint8_t out1 = 3;
 uint8_t out2 = 4;
 uint8_t out3 = 5;
 uint8_t out4 = 6;
+uint8_t front_led = 7;
+uint8_t back_led = 8;
+uint8_t speed_left = 9;
+uint8_t speed_right = 10;
+uint8_t speed[11] = {50, 70, 90, 110, 130, 150, 170, 190, 210, 230, 255}; // speed slider
 //==------------------------------------==
 
 //===================================================
@@ -24,6 +29,10 @@ void setup()
     pinMode(out2, OUTPUT);
     pinMode(out3, OUTPUT);
     pinMode(out4, OUTPUT);
+    pinMode(front_led, OUTPUT);
+    pinMode(back_led, OUTPUT);
+    pinMode(speed_left, OUTPUT);
+    pinMode(speed_right, OUTPUT);
 }
 
 char btSignal = 0;
@@ -32,7 +41,10 @@ void loop()
     if (Serial.available())
     {
         btSignal = Serial.read();
-        Serial.println(btSignal);
+        if (btSignal != 'S')
+        {
+            Serial.println(btSignal);
+        }
         if (btSignal == 'F')
         {
             moveForward();
@@ -69,7 +81,56 @@ void loop()
         {
             stopAllMotors();
         }
-        // else { stop(); } // Remove this if you want to stop.
+        else if (btSignal == 'W')
+        {
+            front_led_ON();
+        }
+        else if (btSignal == 'w')
+        {
+            front_led_OFF();
+        }
+        else if (btSignal == 'V')
+        {
+            back_led_ON();
+        }
+        else if (btSignal == 'v')
+        {
+            back_led_OFF();
+        }
+        else if (btSignal == '0'){
+            sp_0();
+        }
+        else if (btSignal == '1') {
+            sp_1();
+        }
+        else if (btSignal == '2') {
+            sp_2();
+        }
+        else if (btSignal == '3') {
+            sp_3();
+        }
+        else if (btSignal == '4') {
+            sp_4();
+        }
+        else if (btSignal == '5') {
+            sp_5();
+        }
+        else if (btSignal == '6') {
+            sp_6();
+        }
+        else if (btSignal == '7') {
+            sp_7();
+        }
+        else if (btSignal == '8') {
+            sp_8();
+        }
+        else if (btSignal == '9') {
+            sp_9();
+        }
+        else if (btSignal == 'q') {
+            sp_q();
+        }
+        else { stop(); } // Remove this if you want to stop.
     }
 }
 //==------------------------------------==
@@ -151,16 +212,19 @@ void moveForwardLeft()
     spin_right_motors();
     stop_left_motors();
 }
+
 void moveForwardRight()
 {
     spin_left_motors();
     stop_right_motors();
 }
+
 void moveBackwardRight()
 {
     reverse_spin_left_motors();
     stop_right_motors();
 }
+
 void moveBackwardLeft()
 {
     reverse_spin_right_motors();
@@ -172,4 +236,111 @@ void stopAllMotors()
     stop_right_motors();
     stop_left_motors();
 }
+//==-----------------------------==
+
+
+//===============================
+// motor speed
+// -=-
+// No Changes required
+//===============================
+void sp_0()
+{
+    analogWrite(speed_left, speed[0]);
+    analogWrite(speed_right, speed[0]);
+}
+
+void sp_1()
+{
+    analogWrite(speed_left, speed[1]);
+    analogWrite(speed_right, speed[1]);
+}
+
+void sp_2()
+{
+    analogWrite(speed_left, speed[2]);
+    analogWrite(speed_right, speed[2]);
+}
+
+void sp_3()
+{
+    analogWrite(speed_left, speed[3]);
+    analogWrite(speed_right, speed[3]);
+}
+
+void sp_4()
+{
+    analogWrite(speed_left, speed[4]);
+    analogWrite(speed_right, speed[4]);
+}
+
+void sp_5()
+{
+    analogWrite(speed_left, speed[5]);
+    analogWrite(speed_right, speed[5]);
+}
+
+void sp_6()
+{
+    analogWrite(speed_left, speed[6]);
+    analogWrite(speed_right, speed[6]);
+}
+
+void sp_7()
+{
+    analogWrite(speed_left, speed[7]);
+    analogWrite(speed_right, speed[7]);
+}
+
+void sp_8()
+{
+    analogWrite(speed_left, speed[8]);
+    analogWrite(speed_right, speed[8]);
+}
+
+void sp_9()
+{
+    analogWrite(speed_left, speed[9]);
+    analogWrite(speed_right, speed[9]);
+}
+
+void sp_q()
+{
+    analogWrite(speed_left, speed[0]);
+    analogWrite(speed_right, speed[0]);
+}
+
+void stop(){
+    analogWrite(speed_left,0);
+    analogWrite(speed_right, 0);
+}
+
+
+//==-----------------------------==
+
+//===============================
+// Utilities
+// -=-
+// No Changes required
+//===============================
+void front_led_ON()
+{
+    digitalWrite(front_led, HIGH);
+}
+
+void front_led_OFF()
+{
+    digitalWrite(front_led, LOW);
+}
+
+void back_led_ON()
+{
+    digitalWrite(back_led, HIGH);
+}
+
+void back_led_OFF()
+{
+    digitalWrite(back_led, LOW);
+}
+
 //==-----------------------------==
